2204de2d21415d03e48116b0e6cdc49da179fb57
Alexey Bobrov <BobrovAlexeyE@gmail.com>
2020-01-18 12:03:19 +0300

    Added support package

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -1,15 +1,27 @@
 cmake_minimum_required(VERSION 3.10)
 
-option(GTEST_SYSTEM OFF)
-option(BOOST_SYSTEM OFF)
+option(USE_VCPKG "Use vcpkg package manager" ON)
 
-# add external components
-set(COMPONENTS_PREFIX "${CMAKE_CURRENT_SOURCE_DIR}/libs/components")
-set(COMPONENTS_ARCHIVE "${COMPONENTS_PREFIX}/libs")
-set(COMPONENTS_MODULES "${COMPONENTS_PREFIX}/modules")
+if (NOT USE_VCPKG)
 
-list(APPEND CMAKE_MODULE_PATH ${COMPONENTS_MODULES})
+  option(GTEST_SYSTEM OFF)
+  option(BOOST_SYSTEM OFF)
 
-set(Boost_NO_SYSTEM_PATHS ON)
-set(BOOST_INSTALL_DIR ${PROJECT_BINARY_DIR}/stage)
-include(${COMPONENTS_PREFIX}/components.cmake)
+  # add external components
+  set(COMPONENTS_PREFIX "${CMAKE_CURRENT_SOURCE_DIR}/libs/components")
+  set(COMPONENTS_ARCHIVE "${COMPONENTS_PREFIX}/libs")
+  set(COMPONENTS_MODULES "${COMPONENTS_PREFIX}/modules")
+
+  list(APPEND CMAKE_MODULE_PATH ${COMPONENTS_MODULES})
+
+  set(Boost_NO_SYSTEM_PATHS ON)
+  set(BOOST_INSTALL_DIR ${PROJECT_BINARY_DIR}/stage)
+  include(${COMPONENTS_PREFIX}/components.cmake)
+
+else()
+  
+  if (DEFINED ENV{VCPKG_ROOT} AND NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
+  endif()
+
+endif()
--- version.cmake
+++
@@ -1,3 +0,0 @@
-set (MAJOR 1)
-set (MINOR 0)
-set (BUILD 0)
