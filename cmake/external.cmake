346e625937fccf4588a20d626496fc0e184e9123
Alexey Bobrov <BobrovAlexeyE@gmail.com>
2020-05-04 15:25:03 +0300

    added gcov support, build script

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -5,3 +5,11 @@ option(USE_VCPKG "Use vcpkg package manager" ON)
 if (USE_VCPKG)
   set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
 endif()
+
+set(GCOV "$ENV{GCOV}")
+set(LLVM_PROFDATA "$ENV{LLVM_PROFDATA}")
+set(LLVM_COV "$ENV{LLVM_COV}")
+
+message(DEBUG "GCOV=${GCOV}")
+message(DEBUG "LLVM_PROFDATA=${LLVM_PROFDATA}")
+message(DEBUG "LLVM_COV=${LLVM_COV}")
--- package/vcpkg/cmntype/portfile.cmake
+++ package/vcpkg/cmntype/portfile.cmake
@@ -13,13 +13,13 @@ vcpkg_configure_cmake(
   )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/ctlite)
-set(common_DIR lib/cmake/ctlite)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/cmntype)
+set(common_DIR lib/cmake/cmntype)
 
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-configure_file(${SOURCE_PATH}/License.txt ${CURRENT_PACKAGES_DIR}/share/ctlite/copyright COPYONLY)
+configure_file(${SOURCE_PATH}/License.txt ${CURRENT_PACKAGES_DIR}/share/cmntype/copyright COPYONLY)
 
 vcpkg_copy_pdbs()
